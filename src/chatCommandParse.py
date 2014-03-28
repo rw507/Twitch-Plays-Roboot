6e13618954c58599ae9d329b4d3a57fe63cfe581
Ryan <ryan@ubuntu.(none)>
2014-03-28 11:23:09 -0700

    In chatOmmandParse.py, added the class chatTracker

--- src/chatCommandParse.py
+++ src/chatCommandParse.py
@@ -2,6 +2,8 @@
 #Input: List of strings that are chat message from twitch chat
 #Output: The most common valid command from the list
 #   Outputs null if incorrect input or no valid commands
+import random
+
 def parseChatCommands( messages ):
 
     #strip white space in messages and make all lower
@@ -12,6 +14,9 @@ def parseChatCommands( messages ):
 
     #build dictionary of valid commands
     commands = ["forward", "left", "right" , "back"]
+    
+    #for testing on twitch plays pokemon:
+    commands = ["up", 'down', 'left', 'right', 'a', 'b', 'start', 'select']
     commandCounts = {}
     
     #Set count of commands to 0
@@ -33,8 +38,46 @@ def parseChatCommands( messages ):
             commandFrequency = commandCounts[c]
     return mostFrequentCommand
 
-
+class chatTracker:
+        com_track =     {}
+        required = 10
+        def __init__(self, mode = "robot"):
+                
+                if(mode == "pokemon"):
+                       commands = ["up", 'down', 'left', 'right', 'a', 'b', 'start', 'select']
+                elif( mode == "robot"):
+                        commands = ["forward", "left", "right" , "back" ]
+                for c in commands:
+                        self.com_track[c] = 0
+               # print(self.com_track)
+                        
+        def pushMessage(self, m):
+                m = m.lower().replace(" ", '')
+                
+                if( m in self.com_track.keys()):
+                        m_count = self.com_track[m] + 1
+                        if(m_count < self.required):
+                                self.com_track[m] = m_count
+                               # print m_count
+                        else:
+                                for k in self.com_track.keys():
+                                        self.com_track[k] = 0   
+                                return(m)
+                else:
+                        print m + " is not a valid command"
+                       
+        
 
 m = ["AB C","abc", " LEFT", "RIGHT", "RiGhT"]
 
-print(parseChatCommands(m))
+#print(parseChatCommands(m))
+
+
+ct = chatTracker("pokemon")
+commands = ["up", 'down', 'left', 'right', 'a', 'b', 'start', 'select']
+for i in range(0,100):
+       # print("Pushing #: " , i)
+        m = ct.pushMessage(random.choice(commands))
+        
+        if(m):
+                print(m)
